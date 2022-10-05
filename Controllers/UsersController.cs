7f7e3613fcd40539ad0378ce5d53197fe88bf44e
rita <ritavlas@yandex.ru>
2022-10-05 14:56:34 +0300

    Добавьте файлы проекта.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using HW_11.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace HW_11.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string LastName, string FirstName, string MiddleName, Sex Sex, DateTime BirthDate, string Info)
+        {
+            var data = System.IO.File.ReadAllText("Data.json");
+            var users = JsonSerializer.Deserialize<List<UserInfo>>(data) ?? new List<UserInfo>();
+            users.Add(new UserInfo { LastName = LastName, FirstName = FirstName,  MiddleName = MiddleName, BirthDate = BirthDate, Sex = Sex, InformationAboutYourself = Info });
+            data = JsonSerializer.Serialize(users);
+            System.IO.File.WriteAllText("Data.json", data);         
+            return View("Index", "Всё ок!");
+        }
+
+        public IActionResult TakeUsers()
+        {
+            var data = System.IO.File.ReadAllText("Data.json");
+            var users = JsonSerializer.Deserialize<List<UserInfo>>(data) ?? new List<UserInfo>();
+            return View(users);
+        }
+
+        
+
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace HW_11.Models
+{
+    public class UserInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Sex Sex { get; set; }
+        public string InformationAboutYourself { get; set; }
+
+
+    }
+        public enum Sex
+        {
+            Man,
+            Woman
+        }
+
+ }
